f1226206b0e96128d9c84d25d4f1d84da6a23d24
alexe <alexe@W10SSD>
2020-09-30 16:50:08 +0300

    Добавьте файлы проекта.

---
+++ IP_RDS/Program.cs
@@ -0,0 +1,32 @@
+﻿using Cassia;
+using System;
+
+
+namespace IP_RDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITerminalServicesManager manager = new TerminalServicesManager();
+            using (ITerminalServer server = manager.GetLocalServer())
+            {
+                server.Open();
+                foreach (ITerminalServicesSession session in server.GetSessions())
+                    //Console.WriteLine(session.UserAccount.Value);
+                    if (session.UserName == Environment.UserName)
+                    {
+
+                        Console.WriteLine(session.UserAccount + " on session " + session.SessionId + " ip address " + session.ClientIPAddress);
+                    
+            
+                        
+                
+                //Console.WriteLine("It looks like you logged on at " + session.LoginTime + " and are now " + session.ConnectionState); 
+                    } 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
